f4631b392a7791e32c5e19cd1e422b88beefe508
115ek <115ek@users.noreply.github.com>
2015-12-08 09:02:08 +0100

    added input() function

--- SchiffeVersenken.java
+++ SchiffeVersenken.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * Diese Klasse soll das Spiel "Schiffe versenken" simulieren.
  * 
@@ -12,17 +13,17 @@ public class SchiffeVersenken {
      * An dieser Stelle wird das Feld des Spielers mitsamt der Positionen der
      * Schiffe deklariert. '.' = Wasser '#' = Schiff
      */
-    private static char[][] spieler = {{'.', '#', '#', '#', '#', '.', '#', '#', '.', '.'}, // Reihe
+    private static char[][] spieler = { { '.', '#', '#', '#', '#', '.', '#', '#', '.', '.' }, // Reihe
                                                                                               // 0
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '#'}, // Reihe 1
-        {'.', '.', '.', '.', '.', '.', '.', '#', '.', '#'}, // Reihe 2
-        {'#', '#', '#', '.', '#', '.', '.', '#', '.', '#'}, // Reihe 3
-        {'.', '.', '.', '.', '#', '.', '.', '#', '.', '.'}, // Reihe 4
-        {'.', '#', '.', '.', '#', '.', '.', '.', '.', '.'}, // Reihe 5
-        {'.', '#', '.', '.', '#', '.', '.', '#', '#', '.'}, // Reihe 6
-        {'.', '.', '.', '.', '#', '.', '.', '.', '.', '.'}, // Reihe 7
-        {'.', '.', '.', '.', '.', '.', '#', '#', '#', '#'}, // Reihe 8
-        {'.', '#', '#', '.', '.', '.', '.', '.', '.', '.'} // Reihe 9
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '#' }, // Reihe 1
+            { '.', '.', '.', '.', '.', '.', '.', '#', '.', '#' }, // Reihe 2
+            { '#', '#', '#', '.', '#', '.', '.', '#', '.', '#' }, // Reihe 3
+            { '.', '.', '.', '.', '#', '.', '.', '#', '.', '.' }, // Reihe 4
+            { '.', '#', '.', '.', '#', '.', '.', '.', '.', '.' }, // Reihe 5
+            { '.', '#', '.', '.', '#', '.', '.', '#', '#', '.' }, // Reihe 6
+            { '.', '.', '.', '.', '#', '.', '.', '.', '.', '.' }, // Reihe 7
+            { '.', '.', '.', '.', '.', '.', '#', '#', '#', '#' }, // Reihe 8
+            { '.', '#', '#', '.', '.', '.', '.', '.', '.', '.' } // Reihe 9
     };
 
     /**
@@ -30,17 +31,17 @@ public class SchiffeVersenken {
      * der Schiffe nicht bekannt ist, wird fuer alle Felder Wasser ('.')
      * definiert.
      */
-    private static char[][] gegner = {{'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe
+    private static char[][] gegner = { { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe
                                                                                              // 0
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 1
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 2
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 3
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 4
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 5
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 6
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 7
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, // Reihe 8
-        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'} // Reihe 9
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 1
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 2
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 3
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 4
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 5
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 6
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 7
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' }, // Reihe 8
+            { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' } // Reihe 9
     };
 
     /**
@@ -54,7 +55,7 @@ public class SchiffeVersenken {
         int x = 0; // Spalte
         System.out.println("  0123456789"); // Kopfzeile
         while ((x) < 10) {
-            System.out.print((char)(x+65) + " "); // Spaltenbezeichnung
+            System.out.print((char) (x + 65) + " "); // Spaltenbezeichnung
             int y = 0; // Zeile
             while ((y) < 10) {
                 System.out.print(grid[x][y]);
@@ -73,34 +74,19 @@ public class SchiffeVersenken {
      */
     public static void attack() {
         Scanner scn1 = new Scanner(System.in);
-        Scanner scn2 = new Scanner(System.in);
-        int[] shot = new int[2];
-        print(gegner); // Gegnerfeld ausgeben, um vorherige Spielzuege zu
-                       // visualisieren
-        System.out.println("Bitte geben Sie die Koordinaten ein (Form: <X Y> - X=Reihe, Y=Zeile).");
-        shot[0] = scn1.nextInt();
-        shot[1] = scn1.nextInt();
-        if ((shot[0] < 0) || (shot[0] > 9) || (shot[1] < 0) || (shot[1] > 9)) { // falsche
-                                                                                // Werte
-                                                                                // abfangen
-            System.out.println("Fehlerhafte Eingabe");
-        } else { // Trefferabfrage
-            System.out.println("Ziel= " + "Reihe: " + shot[0] + " Spalte: " + shot[1]);
-            System.out.println("Wurde ein Treffer erzielt(y/n)?");
-            String ergebnis;
-            ergebnis = scn2.nextLine();
-            // Ergebnis eintragen
-            if (ergebnis.equals("y")) {
-                gegner[shot[0]][shot[1]] = 'X';
-                System.out.println("Treffer registriert.\n");
-            } else if (ergebnis.equals("n")) {
-                gegner[shot[0]][shot[1]] = 'O';
-                System.out.println("Fehlschuss registriert.\n");
-            } else {
-                System.out.println("Keine gueltige Eingabe.");
-            }
-            print(gegner); // neuen Zustand anzeigen
+        int[] angriff = input();
+        System.out.println("Wurde ein Treffer erzielt(y/n)?");
+        char in = scn1.nextLine().charAt(0);
+        if (in == 'y') {
+            gegner[angriff[0]][angriff[1]] = 'X';
+            System.out.println("Treffer registriert.\n");
+        } else if (in == 'n') {
+            gegner[angriff[0]][angriff[1]] = 'O';
+            System.out.println("Fehlschuss registriert.\n");
+        } else {
+            System.out.println("Keine gueltige Eingabe.");
         }
+        print(gegner);
         return;
     }
 
@@ -109,20 +95,54 @@ public class SchiffeVersenken {
      * damit ein Treffer erzielt wurde, und gibt dies aus.
      */
     public static void defend() {
-        Scanner scn = new Scanner(System.in);
-        int[] verteid = new int[2];
-        System.out.println("Bitte geben Sie die Koordinaten ein (Form: <X Y> - X=Reihe(0-9), Y=Zeile(0-9)):");
-        verteid[0] = scn.nextInt();
-        verteid[1] = scn.nextInt();
+        int[] verteid = input();
         if (spieler[verteid[0]][verteid[1]] == '#') { // Schiff getroffen
             spieler[verteid[0]][verteid[1]] = 'X';
-            System.out.println("Treffer auf " + "Reihe: " + verteid[0] + " Spalte: " + verteid[1] + "\n");
+            System.out.println("Du wurdest getroffen auf " + "Reihe: " + verteid[0] + " Spalte: " + verteid[1] + "\n");
         } else { // verfehlt
             spieler[verteid[0]][verteid[1]] = 'O';
             System.out.println("Verfehlt auf " + "Reihe: " + verteid[0] + " Spalte: " + verteid[1] + "\n");
         }
         print(spieler); // neuen Zustand anzeigen
         return;
+    }
+
+    public static int[] input() {
+        Scanner scn = new Scanner(System.in);
+        int[] koords = new int[2];
+        char shot1, shot2;
+        System.out.println("Bitte geben Sie die Zeile an.");
+        shot1 = scn.nextLine().charAt(0);
+        if ((((int) shot1 > 47) && ((int) shot1 < 58))) {
+            koords[0] = (int) shot1 - 48; // ASCII 0 bis 9
+        } else if ((((int) shot1 > 64) && ((int) shot1 < 75))) {
+            koords[0] = (int) shot1 - 65; // ASCII A bis J
+
+        } else if ((((int) shot1 > 96) && ((int) shot1 < 107))) {
+            koords[0] = (int) shot1 - 97; // ASCII a bis j
+
+        } else {
+            System.out.println("Fehlerhafte Eingabe");
+            koords = input();
+            return koords;
+        }
+
+        System.out.println("Bitte geben Sie die Spalte an.");
+        shot2 = scn.nextLine().charAt(0);
+        if ((((int) shot2 > 47) && ((int) shot2 < 58))) {
+            koords[1] = (int) shot2 - 48; // ASCII 0 bis 9
+        } else if ((((int) shot2 > 64) && ((int) shot2 < 75))) {
+            koords[1] = (int) shot2 - 65; // ASCII A bis J
+
+        } else if ((((int) shot2 > 96) && ((int) shot2 < 107))) {
+            koords[1] = (int) shot2 - 97; // ASCII a bis j
+
+        } else {
+            System.out.println("Fehlerhafte Eingabe");
+            koords = input();
+            return koords;
+        }
+        return koords;
     }
 
     /**
@@ -162,22 +182,22 @@ public class SchiffeVersenken {
             System.out.println("Eingabe: " + eingabe + "\n");
 
             switch (eingabe) {
-                case "attack":// Spieler am Zug
-                    attack();
-                    break;
-                case "defend":// Gegner am Zug
-                    defend();
-                    break;
-                case "exit":// Spiel beenden
-                    System.out.print("Spiel wird beendet...\n");
-                    run = false;
-                    break;
-                case "help":
-                    printMenu();
-                    break;
-                default:// fehlerhafte Eingabe
-                    System.out.println("Keine gueltige Eingabe. <help> fuer mehr Infos.\n");
-                    break;
+            case "attack":// Spieler am Zug
+                attack();
+                break;
+            case "defend":// Gegner am Zug
+                defend();
+                break;
+            case "exit":// Spiel beenden
+                System.out.print("Spiel wird beendet...\n");
+                run = false;
+                break;
+            case "help":
+                printMenu();
+                break;
+            default:// fehlerhafte Eingabe
+                System.out.println("Keine gueltige Eingabe. <help> fuer mehr Infos.\n");
+                break;
             }
         }
         return;
